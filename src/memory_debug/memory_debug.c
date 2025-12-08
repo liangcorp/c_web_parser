9540b092926bd14a33e6b1439f517897b8a4c613
Chen Liang <chen.liang.cn@outlook.com>
2025-12-08 15:28:47 +0800

    add realloc

--- src/memory_debug/memory_debug.c
+++ src/memory_debug/memory_debug.c
@@ -2,8 +2,7 @@
 
 #include "memory_debug.h"
 
-void *f_debug_memory_malloc(unsigned int size, const char *file_name,
-			    unsigned int line_num)
+void *f_debug_memory_malloc(unsigned int size, const char *file_name, unsigned int line_num)
 {
 #undef malloc
 	void *ptr = NULL;
@@ -11,13 +10,13 @@ void *f_debug_memory_malloc(unsigned int size, const char *file_name,
 	ptr = malloc(size);
 
 	if (ptr == NULL) {
-		printf("MEM ERROR: Malloc returns NULL when trying to allocate %u bytes at line %u in file %s\n",
+		printf("MEM ERROR: malloc returns NULL when trying to allocate %u bytes at line %u in file %s\n",
 		       size, line_num, file_name);
 		exit(0);
 	}
 
-	printf("%p malloc %u bytes of memory at line %u in file %s\n", ptr, size,
-	       line_num, file_name);
+	printf("%p malloc %u bytes of memory at line %u in file %s\n", ptr, size, line_num,
+	       file_name);
 	return ptr;
 }
 
@@ -27,13 +26,26 @@ void *f_debug_memory_calloc(unsigned int num, unsigned int size, const char *fil
 #undef calloc
 	void *ptr = calloc(num, size);
 	if (ptr == NULL) {
-		printf("MEM ERROR: Malloc returns NULL when trying to allocate %u bytes at line %u in file %s\n",
+		printf("MEM ERROR: calloc returns NULL when trying to allocate %u bytes at line %u in file %s\n",
 		       size, line_num, file_name);
 		exit(0);
 	}
 	printf("%p calloc %u * %u bytes of memory at line %u in file %s\n", ptr, num, size,
 	       line_num, file_name);
 	return ptr;
+}
+
+void *f_debug_memory_realloc(void *ptr, unsigned int size, const char *file_name, unsigned int line_num)
+{
+#undef realloc
+	void *new_ptr = realloc(ptr, size);
+	if (new_ptr == NULL) {
+		printf("MEM ERROR: realloc returns NULL when trying to allocate %u bytes at line %u in file %s\n",
+		       size, line_num, file_name);
+		exit(0);
+	}
+	printf("%p realloc %u bytes of memory at line %u in file %s\n", new_ptr, size, line_num, file_name);
+	return new_ptr;
 }
 
 void f_debug_memory_free(void *ptr, const char *file_name, unsigned int line_num)
--- src/memory_debug/memory_debug.h
+++ src/memory_debug/memory_debug.h
@@ -6,12 +6,13 @@
 
 #define malloc(size) f_debug_memory_malloc(size, __FILE__, __LINE__)
 #define calloc(num, size) f_debug_memory_calloc(num, size, __FILE__, __LINE__)
+#define realloc(num, size) f_debug_memory_calloc(ptr, size, __FILE__, __LINE__)
 #define free(ptr) f_debug_memory_free(ptr, __FILE__, __LINE__)
 
-void *f_debug_memory_malloc(unsigned int size, const char *file_name,
-			    unsigned int line_num);
+void *f_debug_memory_malloc(unsigned int size, const char *file_name, unsigned int line_num);
 void *f_debug_memory_calloc(unsigned int num, unsigned int size, const char *file_name,
 			    unsigned int line_num);
+void *f_debug_memory_realloc(void *ptr, unsigned int size, const char *file_name, unsigned int line_num);
 void f_debug_memory_free(void *ptr, const char *file_name, unsigned int line_num);
 
 #endif
